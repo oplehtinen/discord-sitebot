0dd8a73f228db83b17dbec0d14ecdc99633f8237
dimits <admin@astraalivankila.net>
2018-05-06 19:39:41 +0300

    First POC
    Outputs .md files based on posts on a Discord channel.
    
    TODO: Front matter

--- test.js
+++ test.js
@@ -1,8 +1,25 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 const client = new Discord.Client();
 
 client.on('ready', () => {
 	console.log('Ready!');
+	const generalChan = client.channels.find('name', 'general');
+	generalChan.fetchMessages({ limit:0 })
+		.then(messages => {
+		//	console.log(messages);
+			messages.array().forEach(message => {
+			//	const arr = message.content.split(' ');
+				console.log(message.author.toString() + ': ' + message.content + message.createdAt);
+				//	const author = message.author.toString();
+				fs.writeFile('posts/' + message.createdTimestamp + '.md', message.content, function(err) {
+					if (err) throw err;
+					console.log('Saved!');
+				});
+
+			});
+		})
+		.catch(console.error);
 });
 
 client.login('NDQyNzA1NzIzNzgxMTUyNzY4.DdCuCQ.DuZEOsPJREyzk59-LV0wUAudNm8');
