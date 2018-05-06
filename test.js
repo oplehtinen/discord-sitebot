802a23873e84469e7b800ff09e3b691cf158c48c
dimits <admin@astraalivankila.net>
2018-05-06 18:40:51 +0300

    add discord.js

---
+++ test.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NDQyNzA1NzIzNzgxMTUyNzY4.DdCuCQ.DuZEOsPJREyzk59-LV0wUAudNm8');
