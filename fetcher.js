255b6198e39d562c6db23ae3f3fc794e7cffddca
dimits <admin@astraalivankila.net>
2018-05-06 19:49:30 +0300

    Move auth token to a separate file
    Added to .gitignore also.

--- fetcher.js
+++ fetcher.js
@@ -1,17 +1,16 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
+const auth = require(auth.token);
 
 client.on('ready', () => {
 	console.log('Ready!');
 	const generalChan = client.channels.find('name', 'general');
 	generalChan.fetchMessages({ limit:0 })
 		.then(messages => {
-		//	console.log(messages);
 			messages.array().forEach(message => {
-			//	const arr = message.content.split(' ');
+
 				console.log(message.author.toString() + ': ' + message.content + message.createdAt);
-				//	const author = message.author.toString();
 				fs.writeFile('posts/' + message.createdTimestamp + '.md', message.content, function(err) {
 					if (err) throw err;
 					console.log('Saved!');
@@ -22,4 +21,4 @@ client.on('ready', () => {
 		.catch(console.error);
 });
 
-client.login('NDQyNzA1NzIzNzgxMTUyNzY4.DdCuCQ.DuZEOsPJREyzk59-LV0wUAudNm8');
+client.login(auth);
