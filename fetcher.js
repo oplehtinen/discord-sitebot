6a8ea2cfbcd89d2ea08aec7d269b7d1748121337
dimits <admin@astraalivankila.net>
2018-05-06 19:55:18 +0300

    Typo

--- fetcher.js
+++ fetcher.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
-const auth = require(auth.token);
+const auth = require('auth.token');
 
 client.on('ready', () => {
 	console.log('Ready!');
