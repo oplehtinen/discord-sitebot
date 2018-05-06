6e6b3a21a79ced696ac582e8ba2b2142c5a0d85d
dimits <admin@astraalivankila.net>
2018-05-07 00:16:54 +0300

    Cleanup url before embedding

--- messagehandler.js
+++ messagehandler.js
@@ -1,20 +1,23 @@
 module.exports = function messageHandler(msg) {
 	msg = msg.toString();
+	let url = msg.match(/\bhttps?:\/\/\S+/gi);
+	if (url !== null) {
+		url = url.toString();
 
+		// YOUTUBE EMBED
+		if (url.includes('youtube.com') || url.includes('youtu.be')) {
+			const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+			const id = url.match(regExp);
+			if (id && id[2].length == 11) {
+				msg = '<iframe width="560" height="315" src="//www.youtube.com/embed/' + id[2] + '" frameborder="0" allowfullscreen></iframe>';
+			}
 
-	// YOUTUBE EMBED
-	if (msg.includes('youtube.com') || msg.includes('youtu.be')) {
-		const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-		const id = msg.match(regExp);
-		if (id && id[2].length == 11) {
-			msg = '<iframe width="560" height="315" src="//www.youtube.com/embed/' + id[2] + '" frameborder="0" allowfullscreen></iframe>';
 		}
 
-	}
-
-	// SPOTIFY embed
-	if (msg.includes('spotify.com')) {
-		msg = '<iframe src="' + msg + '" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+		// SPOTIFY embed
+		if (url.includes('spotify.com')) {
+			msg = msg + '\n<iframe src="' + url + '" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+		}
 	}
 	msg = '---\n---\n' + msg;
 	return msg;
