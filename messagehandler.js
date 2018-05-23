688a7925948f24333ad7ef1b1da0b2db0a37d779
dimits <admin@astraalivankila.net>
2018-05-23 22:52:56 +0300

    Move to Autolinker for safer and more consistent url handling

--- messagehandler.js
+++ messagehandler.js
@@ -1,13 +1,22 @@
 const cheerio = require('cheerio');
 const rp = require('request-promise');
+const Autolinker = require('autolinker');
 
 module.exports = function messageHandler(msg) {
-	msg = msg.toString();
-	let url = msg.match(/\bhttps?:\/\/\S+/gi);
-
-	if (url !== null) {
+	// msg = msg.toString();
+	let url2 = msg.match(/\bhttps?:\/\/\S+/gi);
+	let url = Autolinker.parse(msg, { urls: true, email: false, phone: false });
+	let urltest;
+	if (url2 !== null) {
+		console.log(url.length);
+		for (let i = 0; i < url.length; i++) {
+			// console.log(url[i].getUrl());
+			urltest = url[i].getUrl();
+			// console.log(urltest);
+		}
+		url2 = url2.toString();
 		const options = {
-			uri: url.toString(),
+			uri: urltest,
 			transform: function(body) {
 				return cheerio.load(body);
 			},
@@ -16,21 +25,33 @@ module.exports = function messageHandler(msg) {
 			.then(function($) {
 				const title = $('title').text();
 				url = url.toString();
+				Autolinker.link(msg, {
+					replaceFn : function(match) {
+						const embedThis = match.getUrl();
+						console.log(title, urltest);
 
-				// YOUTUBE EMBED
-				if (url.includes('youtube.com') || url.includes('youtu.be')) {
-					const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-					const id = url.match(regExp);
-					if (id && id[2].length == 11) {
-						msg = msg + '<iframe width="560" height="315" src="//www.youtube.com/embed/' + id[2] + '" frameborder="0" allowfullscreen></iframe>';
-					}
+						if (embedThis.length === msg.length) {
+							msg = '';
+						}
 
-				}
+						if (embedThis.includes('youtube.com') || embedThis.includes('youtu.be')) {
+							const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+							const id = embedThis.match(regExp);
 
-				// SPOTIFY embed
-				if (url.includes('spotify.com')) {
-					msg = msg + '\n<iframe src="' + url + '" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
-				}
+							if (id && id[2].length == 11) {
+								msg = msg + '\n<iframe width="560" height="315" src="//www.youtube.com/embed/' + id[2] + '" frameborder="0" allowfullscreen></iframe>';
+							}
+
+						}
+						// SPOTIFY embed
+						if (embedThis.includes('spotify.com')) {
+							msg = msg + '\n<iframe src="' + embedThis + '" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+							// console.log(title, msg);
+						}
+
+					},
+				});
+
 				return '---\n layout: post \n title: "' + title + '"\n---\n' + msg;
 			});
 	}
