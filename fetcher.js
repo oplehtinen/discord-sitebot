d39f692eacfe6c46de5fee37d19826b2a2566ecc
Olli-Pekka Lehtinen <admin@astraalivankila.net>
2018-11-15 19:58:10 +0200

    Move auth to env from file

--- fetcher.js
+++ fetcher.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
-const auth = require('./authtoken.js');
+// const auth = require('./authtoken.js');
+const auth = process.env.AUTH_TOKEN;
 const messageHandler = require('./messagehandler.js');
 const settings = require('./settings.js');
 
